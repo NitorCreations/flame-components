7c1254346ab3d5b0eb3505e1e5f80bc1b211f439
Juuso Soikkeli <juuso.soikkeli@nitor.fi>
2018-12-21 10:22:50 +0200

    added another test

--- src/components/Button/button.test.tsx
+++ src/components/Button/button.test.tsx
@@ -3,9 +3,21 @@ import { shallow } from 'enzyme';
 import Button from './Button';
 
 describe('Button', () => {
+  it('Should render correctly', () => {
+    const component = shallow(<Button onClick={() => null} />);
+
+    expect(component).toMatchSnapshot();
+  });
+
+  it('Should be correctly enabled', () => {
+    const component = shallow(<Button onClick={() => null} disabled={false} />);
+
+    expect(component).toMatchSnapshot();
+  });
+  
   it('Should be correctly disabled', () => {
     const component = shallow(<Button onClick={() => null} disabled />);
 
-    expect(component.find('button').is('[disabled]')).toBeTruthy();
+    expect(component).toMatchSnapshot();
   });
 });
